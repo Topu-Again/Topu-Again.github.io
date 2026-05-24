c96998947bf9867dbb156e2e62985c9fc3c0c93f
Tanbir Topu <88310175+Topu-Again@users.noreply.github.com>
2026-05-24 20:52:31 +0600

    Add files via upload

---
+++ vsc.js
@@ -0,0 +1,17 @@
+function openNav() {
+    document.getElementById(`sidenav`).style.width = `250px`;
+    document.getElementById(`sidenav`).style.display = `block`;
+}
+
+function closeNav() {
+    document.getElementById(`sidenav`).style.width = `0`;
+}
+
+function openPp() {
+    document.getElementById(`ppcontainer`).style.visibility = `visible`;
+}
+
+function closePp() {
+    document.getElementById(`ppcontainer`).style.visibility = `hidden`;
+}
+
